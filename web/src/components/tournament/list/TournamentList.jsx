5302aca61d70e376fe1d86373633a4b47c3af6ca
Rodrigo <rodicom10@gmail.com>
2024-04-27 15:06:35 -0300

    Boton de crear arreglad

--- web/src/components/tournament/list/TournamentList.jsx
+++ web/src/components/tournament/list/TournamentList.jsx
@@ -1,10 +1,14 @@
+import AddTournamentButton from '../../buttons/AddTournamentButton';
 import SimpleTournament from './simple/SimpleTournament';
 import './TournamentList.css';
 
 const TournamentList = ({ tournaments }) => {
   return (
     <div id="tournament-list">
-      <h3>My Tournaments</h3>
+      <div className="header">
+        <h3>My Tournaments</h3>
+        <AddTournamentButton />
+      </div>
       <div className="grid">
         {tournaments.map((tournament, index) => (
           <SimpleTournament tournament={tournament} key={index} />
